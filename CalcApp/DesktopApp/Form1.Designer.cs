e5f1ca3489aacf80dc9ca197fa47ae1fd0cf7370
nikolay larin <kolyanupasik@gmail.com>
2019-02-13 19:44:11 +0300

    ChangePropertyOfButton

---
+++ CalcApp/DesktopApp/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace CalcApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Dark_RadioButton = new System.Windows.Forms.RadioButton();
+            this.Usual_RadioButton = new System.Windows.Forms.RadioButton();
+            this.Orchid_RadioButton = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(81, 274);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 54);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(150, 274);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(63, 54);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 214);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(63, 54);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(81, 214);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(63, 54);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(150, 214);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(63, 54);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 154);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(63, 54);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(81, 154);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(63, 54);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.Control;
+            this.button9.FlatAppearance.BorderColor = System.Drawing.SystemColors.Window;
+            this.button9.Location = new System.Drawing.Point(150, 154);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(63, 54);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(81, 340);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(63, 54);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "0";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(179, 87);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(82, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // Dark_RadioButton
+            // 
+            this.Dark_RadioButton.AutoSize = true;
+            this.Dark_RadioButton.Location = new System.Drawing.Point(15, 64);
+            this.Dark_RadioButton.Name = "Dark_RadioButton";
+            this.Dark_RadioButton.Size = new System.Drawing.Size(64, 17);
+            this.Dark_RadioButton.TabIndex = 13;
+            this.Dark_RadioButton.TabStop = true;
+            this.Dark_RadioButton.Text = "темный";
+            this.Dark_RadioButton.UseVisualStyleBackColor = true;
+            this.Dark_RadioButton.CheckedChanged += new System.EventHandler(this.Dark_RadioButton_CheckedChanged);
+            // 
+            // Usual_RadioButton
+            // 
+            this.Usual_RadioButton.AutoSize = true;
+            this.Usual_RadioButton.Location = new System.Drawing.Point(15, 45);
+            this.Usual_RadioButton.Name = "Usual_RadioButton";
+            this.Usual_RadioButton.Size = new System.Drawing.Size(81, 17);
+            this.Usual_RadioButton.TabIndex = 12;
+            this.Usual_RadioButton.TabStop = true;
+            this.Usual_RadioButton.Text = "без стилей";
+            this.Usual_RadioButton.UseVisualStyleBackColor = true;
+            this.Usual_RadioButton.CheckedChanged += new System.EventHandler(this.Usual_RadioButton_CheckedChanged);
+            // 
+            // Orchid_RadioButton
+            // 
+            this.Orchid_RadioButton.AutoSize = true;
+            this.Orchid_RadioButton.Location = new System.Drawing.Point(15, 87);
+            this.Orchid_RadioButton.Name = "Orchid_RadioButton";
+            this.Orchid_RadioButton.Size = new System.Drawing.Size(74, 17);
+            this.Orchid_RadioButton.TabIndex = 14;
+            this.Orchid_RadioButton.TabStop = true;
+            this.Orchid_RadioButton.Text = "странный";
+            this.Orchid_RadioButton.UseVisualStyleBackColor = true;
+            this.Orchid_RadioButton.CheckedChanged += new System.EventHandler(this.Orchid_RadioButton_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 406);
+            this.Controls.Add(this.Orchid_RadioButton);
+            this.Controls.Add(this.Dark_RadioButton);
+            this.Controls.Add(this.Usual_RadioButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Калькулятор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton Usual_RadioButton;
+        private System.Windows.Forms.RadioButton Dark_RadioButton;
+        private System.Windows.Forms.RadioButton Orchid_RadioButton;
+    }
+}
+
---
+++ CalcApp/DesktopApp/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcApp
+{
+    /*  Создание делегата и его использование:
+     *  Делегат - тип данных, который может хранить ссылку на метод, 
+     *  сигнатура которого совпадает с сигнатурой, объявленной в делегате.
+     *  1) Объявляем делегат CountDelegat с сигнатурой int(string)
+     *  
+    */
+
+    public partial class Form1 : Form
+    {
+        // Создание списка
+        List<Button> listButtons = new List<Button>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // добавляет в список list ссылки экземпляров класса System.Windows.Forms.Button()
+            listButtons.Add(button1);  
+            listButtons.Add(button2);
+            listButtons.Add(button3);
+            listButtons.Add(button4);
+            listButtons.Add(button5);
+            listButtons.Add(button6); 
+            listButtons.Add(button7);
+            listButtons.Add(button8);
+            listButtons.Add(button9);
+            listButtons.Add(button10);
+        }
+
+        // Меняем свойства кнопок buttons(n) нажатием на переключатель RadioButton
+        private void Usual_RadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Usual_RadioButton.Checked == true)
+            {
+                for (int i = 0; i < listButtons.Count; i++)
+                {
+                    listButtons[i].ForeColor = SystemColors.WindowText;
+                    listButtons[i].BackColor = SystemColors.Control;
+                    //listButtons[i].BackColor = SystemColors.WindowFrame;
+                }
+            }
+        }
+
+        private void Dark_RadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (Dark_RadioButton.Checked == true)
+            {
+                for (int i = 0; i < listButtons.Count; i++)
+                {
+                    listButtons[i].ForeColor = Color.DarkGoldenrod;
+                    listButtons[i].BackColor = Color.DarkBlue;
+                }
+            }            
+        }
+
+        private void Orchid_RadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (Orchid_RadioButton.Checked == true)
+            {
+                for (int i = 0; i < listButtons.Count; i++)
+                {
+                    listButtons[i].ForeColor = Color.Orchid;
+                    listButtons[i].BackColor = Color.YellowGreen;
+                }
+            }
+        }
+    }
+}
